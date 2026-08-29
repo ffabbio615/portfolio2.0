5f6a5098547fa44accbd9d91e71c6118ae4344a8
ffabbio615 <fabiomarques.ads@gmail.com>
2026-08-29 17:31:58 -0300

    Layout do menu de preferências finalizado.

--- src/components/Desktop/TopBar/TopBar.tsx
+++ src/components/Desktop/TopBar/TopBar.tsx
@@ -1,5 +1,5 @@
 import './TopBar.scss';
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function TopBar() {
 
@@ -24,7 +24,7 @@ export default function TopBar() {
       .format(now)
       .replaceAll(".", "")
       .replace(",", "")
-      .replace(/\b\w/g, char => char.toUpperCase());
+      .replace(/(?<=^|\s)\p{L}/gu, char => char.toUpperCase());
     
 
   const [location, setLocation] = useState("Localizando...");
@@ -47,17 +47,37 @@ export default function TopBar() {
                       data.address.village ||
                       data.address.municipality;
 
-                  setLocation(city || "Localização desconhecida");
+                  setLocation(city || "RJ - Brasil");
               } catch (error) {
                   console.error(error);
-                  setLocation("Localização indisponível");
+                  setLocation("RJ - Brasil");
               }
           },
           (error) => {
               console.error(error);
-              setLocation("Localização indisponível");
+              setLocation("RJ - Brasil");
           }
       );
+  }, []);
+
+  const [settingsMenu, setSettingsMenu] = useState<boolean>(false);
+  const settingsRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handlePointerDown = (event: PointerEvent) => {
+        if (
+            settingsRef.current &&
+            !settingsRef.current.contains(event.target as Node)
+        ) {
+            setSettingsMenu(false);
+        }
+    };
+
+    document.addEventListener("pointerdown", handlePointerDown);
+
+    return () => {
+        document.removeEventListener("pointerdown", handlePointerDown);
+    };
   }, []);
 
 
@@ -68,17 +88,45 @@ export default function TopBar() {
       <div className='left-side'>
         <div className='top-bar-logo-container'>
           <img className='top-bar-logo' src='/icon/logo/fm-gray-logo-icon.svg' alt='Logo do Portfólio'/>
-          <h1>Fábio Marques - Portfólio</h1>
+          <h1>Fábio Marques</h1>
         </div>
       </div>
 
-      <div className='middle'>
-        <img className='settings-icon' src='/icon/topbar/settings-icon.svg' alt='Botão para trocar tema claro e escuro'/>
+      <div className='middle' ref={settingsRef}>
+        <button className='settings-main-button' onClick={()=> setSettingsMenu(true)}>
+          <img className={`${settingsMenu ? "settings-icon settings-icon-activated" : "settings-icon" }`} title='Preferências' src='/icon/topbar/settings-icon.svg' alt='Botão para trocar tema claro e escuro'/>
+        </button>
+
+        <nav className={`${settingsMenu ? "settings-menu" : "settings-menu-invisible"}`} aria-label="Configurações do portfólio">
+            <span className="settings-menu-label">Linguagem <span>❯</span></span>
+
+            <ul className="settings-submenu language-submenu">
+                <li>
+                    <button className='settings-submenu-button' type="button" onClick={()=> setSettingsMenu(false)}>Português</button>
+                </li>
+
+                <li>
+                    <button className='settings-submenu-button' type="button" onClick={()=> setSettingsMenu(false)}>English</button>
+                </li>
+            </ul>
+
+            <span className="settings-menu-label">Tema <span>❯</span></span>
+
+            <ul className="settings-submenu theme-submenu">
+                <li>
+                    <button className='settings-submenu-button' type="button" onClick={()=> setSettingsMenu(false)}>Claro</button>
+                </li>
+
+                <li>
+                    <button className='settings-submenu-button' type="button" onClick={()=> setSettingsMenu(false)}>Escuro</button>
+                </li>
+            </ul>
+
+            <button type="button" className="settings-about" onClick={()=> setSettingsMenu(false)}>Sobre o Portfólio</button>
+        </nav>
       </div>
 
       <div className='right-side'>
-        {/* <p className='top-bar-language-button'>Change to English</p> */}
-        {/* <p>Ter 04 Ago 01:20</p> */}
         <time className="topbar-datetime" dateTime={now.toISOString()}>{formattedDate}</time>
         <span className='topbar-location'>{location}</span>
       </div>
